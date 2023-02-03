b32fcd207f4a3dcbeaa70579c4bf123ee8c7e741
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-02-03 10:29:24 +0530

    feat: added product model

---
+++ src/models/product.model.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+import { customAlphabet } from 'nanoid'
+import { string } from 'zod'
+import { UserDocument } from './user.model'
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 10)
+
+export interface productDocument extends mongoose.Document {
+  user: UserDocument['_id']
+  title: string
+  description: string
+  price: number
+  image: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+const productSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => `product_${nanoid()}`,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const productModel = mongoose.model<productDocument>('product', productSchema)
+
+export default productModel
